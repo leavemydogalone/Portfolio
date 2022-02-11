4afba375ea1c1feea2de32dccbdc72878d4d08dd
leavemydogalone <niklasport8*@gmail.com>
2022-02-10 19:05:23 -0800

    more new stuff

--- src/Projects.js
+++ src/Projects.js
@@ -1,28 +1,31 @@
 import React from "react";
 import "./Projects.scss";
-import Project from "./components/Project";
+import GalleryItem from "./components/GalleryItem";
 import { projectObjectArray } from "./TextObjects/projectObjects";
 
 export default function Projects() {
-  const projectObjectArrayCopy = [...projectObjectArray];
+  // const projectObjectArrayCopy = [...projectObjectArray];
 
-  const projectCards = projectObjectArrayCopy.map((projectObject, index) => (
-    <Project
-      title={projectObject.title}
-      icon={projectObject.icon}
-      link={projectObject.link}
-      descriptionArray={projectObject.descriptionArray}
-      key={index}
-    />
-  ));
+  // const projectCards = projectObjectArrayCopy.map((projectObject, index) => (
+  //   <Project
+  //     title={projectObject.title}
+  //     icon={projectObject.icon}
+  //     link={projectObject.link}
+  //     descriptionArray={projectObject.descriptionArray}
+  //     key={index}
+  //   />
+  // ));
 
   return (
     <section className="projects">
       <div className="container">
         <h2 className="small-title">Let's Break it Down</h2>
         <div className="project first">
-          <span className="big-letter">N </span>
-          <h3 className="section-title">Nice Animations</h3>
+          <span className="big-letter">N</span>
+          <h3 className="section-title">Nice Designs</h3>
+          <div className="gallery-container">
+            <div className="gallery-item"></div>
+          </div>
         </div>
       </div>
     </section>
---
+++ src/components/GalleryItem.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "../componentStyles/Project.css";
+
+export default function GalleryItem({ title, image, link }) {
+  return <div className="gallery-item"></div>;
+}
