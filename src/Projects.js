ca7736c334e60b25247522e01fac40e9f58c760d
leavemydogalone <niklasport8*@gmail.com>
2022-03-10 17:01:48 -0800

    second project section added

--- src/Projects.js
+++ src/Projects.js
@@ -1,10 +1,10 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import "./Projects.scss";
 import GalleryItem from "./components/GalleryItem";
 
 export default function Projects() {
   const projectsRef = useRef([]);
-  const projects = document.querySelectorAll(".project");
+  const [inView, setInView] = useState([false, false, false]);
 
   const options = {
     root: null,
@@ -36,7 +36,7 @@ export default function Projects() {
   return (
     <section className="projects">
       <div className="container">
-        <h2 className="title">Who Is Nik?</h2>
+        <h2 className="title">Who is Nik?</h2>
         <h2 className="small-title">Let's Break it Down</h2>
         <div
           className="project first"
@@ -53,6 +53,32 @@ export default function Projects() {
             {standInGallery.map((thing, index) => (
               <GalleryItem title={thing} key={index} />
             ))}
+          </div>
+        </div>
+        <div
+          className="project second"
+          ref={(el) => {
+            projectsRef.current[1] = el;
+          }}
+        >
+          <div className="title-container">
+            <span className="big-letter">I</span>
+            <h3 className="section-title">Interaction and Animation</h3>
+          </div>
+
+          <div className="gallery-container">
+            <div className="image-container">
+              <img src=""></img>
+            </div>
+            <div className="text-container ">
+              <h3 className="smallest-title">About this project</h3>
+              <p className="text">
+                This was created using TypeScript and Reactjs. It is a simple
+                adn short game showing off some custom animations and styles.
+                Exemplary of the kinds of interaction and animation that help
+                make a web page/app come to life.
+              </p>
+            </div>
           </div>
         </div>
       </div>
