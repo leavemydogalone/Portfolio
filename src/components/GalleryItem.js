05a20baa87ed4cb3509d6e6bb7544bfb98b2784a
leavemydogalone <niklasport8*@gmail.com>
2022-03-11 12:06:04 -0800

    new hover effects

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -2,5 +2,11 @@ import React from "react";
 import "../componentStyles/Project.css";
 
 export default function GalleryItem({ title, image, link }) {
-  return <div className="gallery-item">im a thing</div>;
+  return (
+    <div className="gallery-item">
+      <div className="gallery-item-inner">
+        im a thing <a href={link}>Link to {title} website</a>
+      </div>
+    </div>
+  );
 }
