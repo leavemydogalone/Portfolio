ba0e5014e7588af58718a90a073326ae58b1667c
leavemydogalone <niklasport88@gmail.com>
2022-04-17 15:18:54 -0700

    form styles

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -10,14 +10,14 @@ export default function ContactForm() {
   return (
     <form onSubmit={handleSubmit} className="contactForm">
       <h2 class="title">Contact Me</h2>
-      <label htmlFor="name">Name</label>
-      <input id="name" type="Name" name="name" />
+
+      <input id="name" type="Name" name="name" placeholder="Name" />
       <ValidationError prefix="Name" field="name" errors={state.errors} />
-      <label htmlFor="email">Email Address</label>
-      <input id="email" type="email" name="email" />
+
+      <input id="email" type="email" name="email" placeholder="Email" />
       <ValidationError prefix="Email" field="email" errors={state.errors} />
-      <label htmlFor="message">Message</label>
-      <textarea id="message" name="message" />
+
+      <textarea id="message" name="message" placeholder="Message" />
       <ValidationError prefix="Message" field="message" errors={state.errors} />
       <button type="submit" disabled={state.submitting}>
         Submit
